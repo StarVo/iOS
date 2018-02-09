69c8af93cf5fee46de4452832eef90a399da8a31
Jeanie House <jeaniehouse@Jeanies-MacBook-Pro.local>
2018-02-09 14:51:52 -0500

    project change

---
+++ StarVo.tv/StarVo.tv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarVo.tv
+//
+//  Created by Jeanie House on 12/10/17.
+//  Copyright © 2017 Jeanie House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StarVo.tv/StarVo.tv/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StarVo.tv
+//
+//  Created by Jeanie House on 12/10/17.
+//  Copyright © 2017 Jeanie House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
